97593695579eb6aa8c3a8a8f7a16db72f65646d0
EstevanZapata <juanestevanzapatacorrea@gmail.com>
2023-09-06 21:22:22 -0500

    sout

---
+++ Proyecto_poo/src/Proyecto_poo/GradeBook.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto_poo;
+
+/**
+ *
+ * @author juane
+ */
+public class GradeBook {
+    public void displaymessage(String nombre_curso){
+        System.out.println("Holaaaa " + nombre_curso);
+    }
+}
--- Proyecto_poo/src/Proyecto_poo/Proyecto_poo.java
+++ Proyecto_poo/src/Proyecto_poo/Proyecto_poo.java
@@ -2,19 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication20;
+package Proyecto_poo;
 
 /**
  *
  * @author juane
  */
-public class JavaApplication20 {
+public class Proyecto_poo {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GradeBook mensaje = new GradeBook();
+        mensaje.displaymessage("pepe");
     }
     
 }
